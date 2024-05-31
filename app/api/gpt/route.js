68bbd0e4407861e4d3edfd8f8489a16f7209b9a1
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2024-05-31 20:30:10 +0530

    ok

--- app/api/gpt/route.js
+++ app/api/gpt/route.js
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
-const openai = new OpenAI({});
+const openai = new OpenAI();
 
 export async function POST(request) {
 	const {prompt} = await request.json();
-	console.log(prompt);
 	const guide = "You will be provided with a text, and your task is to answer the query in detail, with examples. You should also provide sample code snippets wherever necessary."
 	const constructed_prompt = guide + prompt
 	const completion = await openai.chat.completions.create({
@@ -13,6 +12,5 @@ export async function POST(request) {
 		model: "gpt-3.5-turbo",
 	});
 
-	console.log(completion.choices[0]);
 	return NextResponse.json({ content: completion.choices[0].message.content }, { status: 200 });
 }
